104e0d48eb5c88997a3361e27f5778eed6a017c0
Andrew Liang <liang.y.andrew@gmail.com>
2021-10-21 16:12:22 -0700

    another taco shack commit

--- taco_shack.py
+++ taco_shack.py
@@ -1,88 +1,88 @@
-total_days = 298
+total_days = 377
 total_hours = total_days * 24
-work_rate = 3623 / total_hours
-tip_rate = 4119 / total_hours
+work_rate = (4482 + 2596 * 2) / total_hours
+tip_rate = 5066 / total_hours
 
 
 ShackUpgrades = {}
 
 BeachUpgrades = {
-    'paint'      : (361000   , 10),
-    'furniture'  : (693600   , 20),
-    'bathrooms'  : (871200   , 25),
+    'paint'      : (380250   , 10),
+    # 'furniture'  : (735000   , 20),
+    'bathrooms'  : (980000   , 25),
     # 'billboard'  : (900000   , 35),
     # 'appliances' : (1153200  , 20 * work_rate),
     # 'tipjar'     : (648000   , 50 * tip_rate),
 
-    'decals'     : (1805000  , 50),
+    'decals'     : (2000000  , 50),
     # 'wheels'     : (1912500  , 100),
     # 'mixers'     : (5625000  , 250),
     # 'server'     : (0000000  , 400),
-    'freezer'    : (27000000 , 750),
+    'freezer'    : (36750000 , 750),
 
     # 'shells'     : (160000   , 5),
-    'umbrella'   : (342250   , 10),
-    'leis'       : (1093500  , 30),
+    'umbrella'   : (380250   , 10),
+    'leis'       : (1176000  , 30),
     # 'tanks'      : (540000   , 125),
     # 'fountain'   : (13500000 , 500),
 
-    'apprentice' : (342250   , 10),
-    'cook'       : (693600   , 20),
-    'advertiser' : (716800   , 20),
-    'greeter'    : (871200   , 25),
-    'sous'       : (1387200  , 40),
-    'head'       : (2312000  , 65),
-    'executive'  : (5120000  , 150),
+    'apprentice' : (380250   , 10),
+    'cook'       : (777600   , 20),
+    'advertiser' : (762300   , 20),
+    'greeter'    : (924800   , 25),
+    'sous'       : (1555200  , 40),
+    'head'       : (2450000  , 65),
+    'executive'  : (5780000  , 150),
 
-    'newspaper'  : (358400   , 10),
-    'radio'      : (707850   , 20),
-    'email'      : (1024000  , 30),
-    'internet'   : (1800000  , 50),
+    'newspaper'  : (381150   , 10),
+    'radio'      : (751400   , 20),
+    'email'      : (1156000  , 30),
+    'internet'   : (1922000  , 50),
     # 'tv'         : (2200000  , 160),
     # 'blimp'      : (4000000  , 200),
 }
 
 CityUpgrades = {
-    'paint'      : (210250   , 10),
-    'furniture'  : (405600   , 20),
-    'bathrooms'  : (540800   , 25),
-    'billboard'  : (729000   , 35),
-    'appliances' : (235200   , 20 * work_rate),
-    'tipjar'     : (612500   , 50 * tip_rate),
+    'paint'      : (240250   , 10),
+    'furniture'  : (504600   , 20),
+    'bathrooms'  : (627200   , 25),
+    'billboard'  : (841000   , 35),
+    'appliances' : (529200   , 20 * work_rate),
+    # 'tipjar'     : (612500   , 50 * tip_rate),
 
-    'buns'       : (607500   , 50),
-    'condiments' : (1000000  , 100),
-    'beverages'  : (3200000  , 275),
-    'coolers'    : (6250000  , 450),
-    'grill'      : (9000000  , 800),
+    'buns'       : (1267500  , 50),
+    'condiments' : (2560000  , 100),
+    'beverages'  : (7200000  , 275),
+    'coolers'    : (12250000 , 450),
+    'grill'      : (25000000 , 800),
 
-    'banner'     : (101400   , 5),
-    'sign'       : (220500   , 10),
-    'glass'      : (640000   , 30),
+    'banner'     : (126150   , 5),
+    'sign'       : (242000   , 10),
+    'glass'      : (722500   , 30),
     'artwork'    : (3600000  , 150),
     'chandelier' : (20000000 , 500),
 
-    'apprentice' : (210250   , 10),
-    'cook'       : (405600   , 20),
-    'advertiser' : (437500   , 20),
-    'greeter'    : (540800   , 25),
-    'sous'       : (811200   , 40),
-    'head'       : (1352000  , 65),
-    'executive'  : (3125000  , 150),
+    'apprentice' : (240250   , 10),
+    'cook'       : (504600   , 20),
+    'advertiser' : (510300   , 20),
+    'greeter'    : (627200   , 25),
+    'sous'       : (1009200  , 40),
+    'head'       : (1568000  , 65),
+    'executive'  : (3645000  , 150),
 
-    'newspaper'  : (218750   , 10),
-    'radio'      : (406250   , 20),
-    'email'      : (625000   , 30),
-    'internet'   : (1058000  , 50),
-    # 'tv'         : (2200000  , 160),
-    # 'blimp'      : (4000000  , 200),
+    'newspaper'  : (255150   , 10),
+    'radio'      : (509600   , 20),
+    'email'      : (729000   , 30),
+    'internet'   : (1250000  , 50),
+    'tv'         : (4009500  , 160),
+    'blimp'      : (6250000  , 200),
 }
 
 FranchiseUpgrades = {
     'drivethru' : (100000 , 1000),
-    'customer'  : (40000  , .1 * tip_rate * 14000),
-    'food'      : (40000  , .1 * work_rate * 14000),
-    'overtime'  : (100000 , .1 * work_rate * 14000 * 2),
+    'customer'  : (62500  , .1 * tip_rate * 14000),
+    'food'      : (62500  , .1 * work_rate * 14000),
+    'overtime'  : (144000 , .1 * work_rate * 14000 * 2),
     'marketing' : (45000  , 300),
     'hq'        : (40000  , 500),
 }
