ca4f37b72bc41717debf8dc8573477eef65f46a6
Andrew Liang <liang.y.andrew@gmail.com>
2022-02-10 17:10:25 -0800

    taco shack update

--- taco_shack.py
+++ taco_shack.py
@@ -7,31 +7,18 @@ tip_rate = 5066 / total_hours
 ShackUpgrades = {}
 
 BeachUpgrades = {
-    'decals'     : (3125000  , 50),
+    # 'decals'     : (3125000  , 50),
     # 'wheels'     : (1912500  , 100),
     # 'mixers'     : (5625000  , 250),
     # 'server'     : (0000000  , 400),
     'freezer'    : (48000000 , 750),
-
-    # 'shells'     : (160000   , 5),
-    # 'umbrella'   : (400000   , 10),
-    'leis'       : (1734000  , 30),
-    # 'tanks'      : (540000   , 125),
-    # 'fountain'   : (13500000 , 500),
-
-    # 'newspaper'  : (560000   , 10),
-    # 'radio'      : (796250   , 20),
-    # 'email'      : (1225000  , 30),
-    'internet'   : (2888000  , 50),
-    # 'tv'         : (2200000  , 160),
-    # 'blimp'      : (4000000  , 200),
 }
 
 CityUpgrades = {
-    'paint'      : (462250   , 10),
-    'furniture'  : (912600   , 20),
-    'bathrooms'  : (1155200  , 25),
-    'billboard'  : (1600000  , 35),
+    'paint'      : (484000   , 10),
+    'furniture'  : (960000   , 20),
+    'bathrooms'  : (1216800  , 25),
+    # 'billboard'  : (1600000  , 35),
     'appliances' : (1009200  , 20 * work_rate),
     # 'tipjar'     : (612500   , 50 * tip_rate),
 
@@ -39,26 +26,26 @@ CityUpgrades = {
     'condiments' : (4840000  , 100),
     'beverages'  : (12800000 , 275),
     'coolers'    : (25000000 , 450),
-    'grill'      : (36000000 , 800),
+    'grill'      : (49000000 , 800),
 
-    'banner'     : (228150   , 5),
-    'sign'       : (450000   , 10),
+    'banner'     : (240000   , 5),
+    'sign'       : (480500   , 10),
     'glass'      : (1440000  , 30),
     # 'artwork'    : (6400000  , 150),
     'chandelier' : (45000000 , 500),
 
-    'apprentice' : (462250   , 10),
-    'cook'       : (912600   , 20),
-    'advertiser' : (907200   , 20),
-    'greeter'    : (1155200  , 25),
-    'sous'       : (1825200  , 40),
+    'apprentice' : (484000   , 10),
+    'cook'       : (960000   , 20),
+    'advertiser' : (958300   , 20),
+    'greeter'    : (1216800  , 25),
+    'sous'       : (1920000  , 40),
     'head'       : (3042000  , 65),
-    'executive'  : (6845000  , 150),
+    'executive'  : (7220000  , 150),
 
-    'newspaper'  : (453600   , 10),
+    'newspaper'  : (479150   , 10),
     'radio'      : (938600   , 20),
-    'email'      : (1369000  , 30),
-    'internet'   : (2312000  , 50),
+    'email'      : (1444000  , 30),
+    'internet'   : (2450000  , 50),
     # 'tv'         : (4950000  , 160),
     # 'blimp'      : (6250000  , 200),
 }
